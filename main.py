2dea51102ea2e7fa8b19dbfadd8b928610c084ad
zweatshirt <>
2021-05-22 06:30:36 -0500

    first commit, I imagine pushing the entire venv is a bad habit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pandas import DataFrame
 # from shutil import copy
 from openpyxl import Workbook
 from openpyxl import load_workbook
+import traceback as tb
 
 
 def main():
@@ -13,8 +14,17 @@ def main():
     print(date)
 
     # Random testing
-    for i in range(1, len(df_dict)):
-        print(df_dict[i]['Employee'])
+    cols = list(x for x in df_dict[0])
+    # all rows
+    for i in range(0, len(df_dict)):
+        if i == 0:
+            print(cols[0] + "      " + cols[1] + "     " + cols[2])
+        employee = df_dict[i][cols[0]]
+        job = df_dict[i][cols[1]]
+        shift = df_dict[i][cols[2]]
+        print(employee, job, shift)
+
+
 
     # Grab name to save new excel file as:
     xlsx_name = str(input("What would you like to name the excel file?\n"))
@@ -33,7 +43,8 @@ def pdf_to_dict():
             df = DataFrame(rp(pdf_name, pages='all')[0])
             df_dict = df.to_dict('index')
             break
-        except IOError:
+        except Exception:
+            tb.print_exc()
             print("Error parsing PDF, please try again.")
 
     return df_dict
